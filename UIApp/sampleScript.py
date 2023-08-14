f2ec7cc4de42bee53ab5468b71a8c97d9b698bbf
unknown <22abhimishra1993@gmail.com>
2023-08-14 20:03:27 +0530

    Edit button added

--- UIApp/sampleScript.py
+++ UIApp/sampleScript.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 
 label = sg.Text("Let's Play KBC!!!!")
 addBtn = sg.Button("Add")
+editBtn = sg.Button("Edit Me")
 submitBtn = sg.Button("Submit Btn")
-window = sg.Window(title="Hello Audience", layout=[[label], [addBtn], [submitBtn]])
+window = sg.Window(title="Hello Audience", layout=[[label], [addBtn, editBtn], [submitBtn]])
 window.read()
