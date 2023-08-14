6df8b0859837f4991d39feb03c1e78ce474f52cb
unknown <22abhimishra1993@gmail.com>
2023-08-14 19:05:16 +0530

    Initial Commit

---
+++ UIApp/sampleScript.py
@@ -0,0 +1,7 @@
+import PySimpleGUI as sg
+
+label = sg.Text("Let's Play KBC!!!!")
+addBtn = sg.Button("Add")
+submitBtn = sg.Button("Submit")
+window = sg.Window(title="Hello Audience", layout=[[label], [addBtn], [submitBtn]])
+window.read()
