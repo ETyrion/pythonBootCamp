a45d2a1ee4c75870bbada8d904351a887c5f32ed
unknown <22abhimishra1993@gmail.com>
2023-08-15 13:05:17 +0530

    Added UI elements

--- UIApp/sampleScript.py
+++ UIApp/sampleScript.py
@@ -1,10 +1,23 @@
+from BackendCode import addToDoItems as addFunc
+from BackendCode import getToDos as getFunc
+
 import PySimpleGUI as sg
 
-label = sg.Text("***** Let's Play KBC ******")
-addBtn = sg.Button("Add")
-editBtn = sg.Button("Edit Me")
-submitBtn = sg.Button("Submit Btn")
-cancelBtn = sg.Button("Cancel Btn")
-name = sg.Input("Name")
+label = sg.Text("To Do App")
+addBtn = sg.Button("Add New To Do")
+editBtn = sg.Button("Edit To Do")
+submitBtn = sg.Button("Submit")
+cancelBtn = sg.Button("Cancel")
+name = sg.InputText(tooltip="Name", key='Name')
 window = sg.Window(title="Hello Audience", layout=[[label], [addBtn, editBtn],[name], [submitBtn, cancelBtn]])
-window.read()
+event, values = window.read()
+print(event)
+print(values['Name'])
+
+if event == 'Submit':
+    toDoList = getFunc.getToDoList()
+    print(toDoList)
+    addToDoList = values['Name'] + "\n"
+    print(addToDoList)
+    toDoList.append(addToDoList)
+    addFunc.addToDoList(toDoList)
