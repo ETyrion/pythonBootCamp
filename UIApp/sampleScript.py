1d413883dea8b7cce140406a164bb62140621bfc
ETyrion <58480948+ETyrion@users.noreply.github.com>
2023-08-15 09:01:29 +0530

    Delete feature (#3)
    * Added cancelButton
    
    * Updated label

--- UIApp/sampleScript.py
+++ UIApp/sampleScript.py
@@ -1,10 +1,10 @@
 import PySimpleGUI as sg
 
-label = sg.Text("Let's Play KBC!!!!")
+label = sg.Text("***** Let's Play KBC ******")
 addBtn = sg.Button("Add")
 editBtn = sg.Button("Edit Me")
 submitBtn = sg.Button("Submit Btn")
-deleteBtn = sg.Button("Delete Btn")
+cancelBtn = sg.Button("Cancel Btn")
 name = sg.Input("Name")
-window = sg.Window(title="Hello Audience", layout=[[label], [addBtn, editBtn],[name], [submitBtn, deleteBtn]])
+window = sg.Window(title="Hello Audience", layout=[[label], [addBtn, editBtn],[name], [submitBtn, cancelBtn]])
 window.read()
